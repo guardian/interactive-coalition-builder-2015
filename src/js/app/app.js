e650d6f4ae7fa0b42937128b29f86097cd0831d2
pomme <apple.chan-fardel@guardian.co.uk>
2015-05-05 17:00:26 +0100

    tweek on Xaquin and Troy's review and try scroll animation

--- src/js/app/app.js
+++ src/js/app/app.js
@@ -3,47 +3,44 @@ define([
     'app/Builder',
     'app/Coalition',
     'app/updateData',
-    'common/utilities',
+    'common/animate.js',
     'common/buttons.js'
 ], function(
     d3,
     Builder,
     yourCoalition,
     updateData,
-    util,
+    animateScroll,
     socialButtons
 ) {
     'use strict';
 
 
     function render(rawData) {
+        //TODO: data and date will be fixed after the election result comes out
 
-        // window size
-        var windowSize = util.getWindowSize(),
-            width = windowSize.width || 320;
-
-        var r = width / 2 - 32, //radius of octagon
-            cx = r + 20,
-            cy = r - 15;
-        
         /* data */
-        var sumData = rawData.sheets.SUM,
+        var date = rawData.sheets.LASTUPDATE[0],
             textData = rawData.sheets.TEXT,
+            sumData = rawData.sheets.SUM,
             analysisData = rawData.sheets.RESULT,
             partyData = updateData.initParties(sumData, analysisData);
 
 
         /* view */
+
         // section 1
         d3.select(".js-headline").textContent = textData[0].headline;
         d3.select(".js-standfirst").textContent = textData[0].context;
         
-        //TODO: check the date
-        var lastupdateData = new Date(rawData.updated),
+        var //lastupdateData = new Date(rawData.updated),
+            lastupdateData = new Date(date.currentdate + " " + date.currenttime),
             textLastupdate = lastupdateData.toString();
-
+        
         textLastupdate = textLastupdate.slice(4, -18) + " " + textLastupdate.slice(-4, -1);
         document.querySelector("#jsLastUpdate").textContent = "Last update on " + textLastupdate;
+        //console.log(date);
+        //console.log(new Date(date.currentdate + " "  + date.currenttime));
 
         // section 2
         var builder=new Builder({
@@ -51,13 +48,22 @@ define([
             playground:"#playground",
             data:partyData
         });
-        
-        //TODO: check position
-        //builder.getPickMe()
+          
         d3.select(".js-pickme")
-            .style("top", 130 + "px")
-            .classed("animate-delay", true);
-
+        .style("top", 130 + "px")
+        .classed("animate-delay", true);
+        
+        /*var el = document.querySelector(".js-btn-done"), 
+            anchor  = document.querySelector(".js-coalition"); 
+        el.addEventListener("click",runScroll,false);
+        function runScroll() {
+        animateScroll(
+            el,
+            anchor.offsetTop,
+            1000
+        );}*/
+        
+        
         // section 3
         yourCoalition(partyData, analysisData);    
         socialButtons();
